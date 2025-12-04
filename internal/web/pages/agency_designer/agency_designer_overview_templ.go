6df188ab6d4467f9e738a61d8a7d4f61e9bb07d5
Anthony Mugwanga <amosanya@gmail.com>
2025-12-04 12:43:49 +0000

    fix: Update work items section styling for consistency in overview content

--- internal/web/pages/agency_designer/agency_designer_overview_templ.go
+++ internal/web/pages/agency_designer/agency_designer_overview_templ.go
@@ -59,7 +59,7 @@ func OverviewContent(currentAgency *models.Agency, conversation *ai.Conversation
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div><div id=\"content-work-items\" class=\"overview-content-section\" style=\"display: none;\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div><div id=\"content-work-items\" class=\"overview-content-section m-0 p-0\" style=\"display: none;\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
